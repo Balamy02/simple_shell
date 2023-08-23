051e5dea31b12122ff4100e9eedfe352d927d899
BALAMY <ahmedbalamypro@gmail.com>
2023-08-23 15:04:42 -0700

    C - Simple Shell

---
+++ style_checker.c
@@ -0,0 +1,19 @@
+#include "header_file.h"
+
+/**
+ * main - Entry point for the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int variable_name;
+
+    variable_name = 0;
+
+    /* This is a comment explaining what this code does */
+    printf("Hello, world!\n");
+
+    return (0);
+}
+
